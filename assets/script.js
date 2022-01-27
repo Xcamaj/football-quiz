8b5aea15d00cbf8da75bfac2753a2d2f712072cd
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-01-27 12:56:22 -0500

    added questions

--- assets/script.js
+++ assets/script.js
@@ -1,193 +1,111 @@
 var questions = [
-    {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts",
-    },
-    {
-      question:
-        "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses",
-    },
-  ];
-  
-  var questionEl = document.querySelector("#question");
-  var optionListEl = document.querySelector("#option-list");
-  var questionResultEl = document.querySelector("#question-result");
-  var timerEl = document.querySelector("#timer");
-  
-  var questionIndex = 0;
-  var correctCount = 0;
-  
-  function renderQuestion() {
-    questionEl.textContent = questions[questionIndex].question;
-  
-    optionListEl.innerHTML = "";
-  
-    var choices = questions[questionIndex].choices;
-    var choicesLength = choices.length;
-  
-    for (var i = 0; i < choicesLength; i++) {
-      var questionListItem = document.createElement("li");
-      questionListItem.textContent = choices[i];
-      optionListEl.append(questionListItem);
-    }
-}
-  
-  document
-    .querySelector("#change-question")
-    .addEventListener("click", function () {
-      questionIndex++;
-      renderQuestion();
-    });
+  {
+    question: "How many points is a touchdown:",
+    choices: ["7", "6", "1", "3"],
+    answer: "6",
+  },
+  {
+    question: "How many points is a safety",
+    choices: ["2", "1", "8", "3"],
+    answer: "2",
+  },
+  {
+    question: "Who starts with the football on offense",
+    choices: ["Runningback", "Wide Receiver", "Center", "Quarterback"],
+    answer: "Center",
+  },
+  {
+    question: "How many players are allowed on the field for each team",
+    choices: ["10", "22", "7", "11"],
+    answer: "11",
+  },
+  {
+    question: "What is the final game of the NFL season called",
+    choices: ["The Finale", "The Championships", "The Superbowl", "The Cup"],
+    answer: "The Super Bowl"
+  }
+];
 
-    var questions = [
-        {
-          question: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts",
-        },
-        {
-          question:
-            "The condition in an if / else statement is enclosed within ____.",
-          choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-          answer: "parentheses",
-        },
-      ];
-      
-      var questionEl = document.querySelector("#question");
-      var optionListEl = document.querySelector("#option-list");
-      var questionResultEl = document.querySelector("#question-result");
-      var timerEl = document.querySelector("#timer");
-      
-      var questionIndex = 0;
-      var correctCount = 0;
-      
-      function renderQuestion() {
-        questionEl.textContent = questions[questionIndex].question;
-      
-        optionListEl.innerHTML = "";
-        questionResultEl.innerHTML = "";
-      
-        var choices = questions[questionIndex].choices;
-        var choicesLenth = choices.length;
-      
-        for (var i = 0; i < choicesLenth; i++) {
-          var questionListItem = document.createElement("li");
-          questionListItem.textContent = choices[i];
-          optionListEl.append(questionListItem);
-        }
-      }
-      
-      function nextQuestion() {
-        questionIndex++;
-        renderQuestion();
-      }
-      
-      function checkAnswer(event) {
-        if (event.target.matches("li")) {
-          var answer = event.target.textContent;
-          if (answer === questions[questionIndex].answer) {
-            questionResultEl.textContent = "Correct";
-            correctCount++;
-          } else {
-            questionResultEl.textContent = "Incorrect";
-          }
-        }
-        setTimeout(nextQuestion, 2000);
-      }
-      
-      optionListEl.addEventListener("click", checkAnswer);
-      
-      renderQuestion();
-      var questions = [
-        {
-          question: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts",
-        },
-        {
-          question:
-            "The condition in an if / else statement is enclosed within ____.",
-          choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-          answer: "parentheses",
-        },
-      ];
-      
-      var questionEl = document.querySelector("#question");
-      var optionListEl = document.querySelector("#option-list");
-      var questionResultEl = document.querySelector("#question-result");
-      var timerEl = document.querySelector("#timer");
-      
-      var questionIndex = 0;
-      var correctCount = 0;
-      
-      var time = 20;
-      var intervalId;
-      
-      function endQuiz() {
-        clearInterval(intervalId);
-        var body = document.body;
-        body.innerHTML = "Game over, You scored " + correctCount;
-      }
-      
-      function updateTime() {
-        time--;
-        timerEl.textContent = time;
-        if (time <= 0) {
-          endQuiz();
-        }
-      }
-      
-      function renderQuestion() {
-        
-        if (time == 0) {
-          updateTime();
-          return;
-        }
-      
-        intervalId = setInterval(updateTime, 1000);
-        
-        questionEl.textContent = questions[questionIndex].question;
-      
-        optionListEl.innerHTML = "";
-        questionResultEl.innerHTML = "";
-      
-        var choices = questions[questionIndex].choices;
-        var choicesLenth = choices.length;
-      
-        for (var i = 0; i < choicesLenth; i++) {
-          var questionListItem = document.createElement("li");
-          questionListItem.textContent = choices[i];
-          optionListEl.append(questionListItem);
-        }
-      }
-      
-      function nextQuestion() {
-        questionIndex++;
-        if (questionIndex === questions.length) {
-          time = 0;
-        }
-        renderQuestion();
-      }
-      
-      function checkAnswer(event) {
-        clearInterval(intervalId);
-        if (event.target.matches("li")) {
-          var answer = event.target.textContent;
-          if (answer === questions[questionIndex].answer) {
-            questionResultEl.textContent = "Correct";
-            correctCount++;
-          } else {
-            questionResultEl.textContent = "Incorrect";
-            time = time - 2;
-            timerEl.textContent = time;
-          }
-        }
-        setTimeout(nextQuestion, 2000);
-      }
-      
-      renderQuestion();
-      optionListEl.addEventListener("click", checkAnswer);
-      
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
+
+var questionIndex = 0;
+var correctCount = 0;
+
+var time = 60;
+var intervalId;
+
+function renderQuestion() {
+
+  if (time == 0) {
+    updateTime();
+    return;
+  }
+
+  intervalId = setInterval(updateTime, 1000);
+
+  questionEl.textContent = questions[questionIndex].question;
+
+  optionListEl.innerHTML = "";
+  questionResultEl.innerHTML = "";
+
+  var choices = questions[questionIndex].choices;
+  var choicesLenth = choices.length;
+
+  for (var i = 0; i < choicesLenth; i++) {
+    var questionListItem = document.createElement("li");
+    questionListItem.textContent = choices[i];
+    optionListEl.append(questionListItem);
+  }
+}
+
+function endQuiz() {
+  clearInterval(intervalId);
+  var body = document.body;
+  body.innerHTML = "Game over, You scored " + correctCount;
+}
+
+function updateTime() {
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
+document
+  .querySelector("#change-question")
+  .addEventListener("click", function () {
+    questionIndex++;
+    renderQuestion();
+  });
+
+
+function nextQuestion() {
+  questionIndex++;
+  if (questionIndex === questions.length) {
+    time = 0;
+  }
+  renderQuestion();
+}
+
+function checkAnswer(event) {
+  clearInterval(intervalId);
+  if (event.target.matches("li")) {
+    var answer = event.target.textContent;
+    if (answer === questions[questionIndex].answer) {
+      questionResultEl.textContent = "Correct";
+      correctCount++;
+    } else {
+      questionResultEl.textContent = "Incorrect";
+      time = time - 2;
+      timerEl.textContent = time;
+    }
+  }
+  setTimeout(nextQuestion, 2000);
+}
+
+renderQuestion();
+optionListEl.addEventListener("click", checkAnswer);
